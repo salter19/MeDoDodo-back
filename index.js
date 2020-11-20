745f7388e5c18ef9f645d1b835686c3ea3c94eaa
haelina <56834622+haelina@users.noreply.github.com>
2020-11-20 13:17:29 +0200

    Modify index.js.
    *Add datadase connection as variable database.
    *Add router as variable tasks.
    *Change server function to async/await.
    *Add error handling if server fails to operate.

---
+++ index.js
@@ -0,0 +1,23 @@
+const database = require("./routes/promisecrud.js");
+const tasks = require("./routes/tasks.js");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+// const db = [{ name: "tiina" }, { name: "jack" }];
+const port = process.env.PORT || 8080;
+
+// connect router to app
+app.use("/tasks", tasks);
+
+// open server and catch errors
+const server = app.listen(port, async () => {
+  try {
+    await database.connect();
+    console.log(`Listening on port ${server.address().port}`);
+  } catch (err) {
+    console.log(err);
+    server.close();
+  }
+});
